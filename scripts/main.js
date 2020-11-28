0fc3783732bb5dc7deea52216f9dde156e9244c3
Jopt <jopt0598@hotmail.com>
2020-11-28 17:00:18 -0600

    Changes on animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,44 @@
 
-const servicesBoxes = document.querySelectorAll('.services__box');
+const servicesBoxes = document.querySelectorAll('.services__box'),
+appearingText 		= document.querySelector('.subtitle');
 
 window.addEventListener('scroll', function() {
-	var element = servicesBoxes[0];
-	var position = element.getBoundingClientRect();
+	const element = servicesBoxes[0];
+	const position = element.getBoundingClientRect();
 
-	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+	if(position.top < 500 && position.top > 0 ) {
         element.classList.add('animate__animated');
         element.classList.add('animate__bounceInLeft');
 	}
 });
 
 window.addEventListener('scroll', function() {
-	var element = servicesBoxes[1];
-	var position = element.getBoundingClientRect();
+	const element = servicesBoxes[1];
+	const position = element.getBoundingClientRect();
 
-	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+	if(position.top < 500 && position.top > 0 ) {
         element.classList.add('animate__animated');
         element.classList.add('animate__bounceInLeft');
 	}
 });
 
 window.addEventListener('scroll', function() {
-	var element = servicesBoxes[2];
-	var position = element.getBoundingClientRect();
+	const element = servicesBoxes[2];
+	const position = element.getBoundingClientRect();
 
-	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+	if(position.top < 500 && position.top > 0 ) {
         element.classList.add('animate__animated');
         element.classList.add('animate__bounceInLeft');
 	}
 });
+
+const writeText = () => {
+	let string = 'FRONTEND DEVELOPER AND MECHATRONICS ENGINEER';
+
+	for(let char of string){
+		appearingText.innerHTML += char;
+	}
+}
+
+writeText();
+
